f2fb62a22cba6c913b9fea64003d37686311181d
Imsk31 <121716970+Imsk31@users.noreply.github.com>
2026-03-27 17:40:15 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,7 @@ terraform {
     region = "us-east-1"
     bucket = "shubham-terraform-bucket-042831095536"
     key = "statefile/terraform.tfstate"
-    dynamodb_table = "terraform-state-locks"
-    # use_lockfile = true
+    use_lockfile = true
     encrypt = true
   }
 }
