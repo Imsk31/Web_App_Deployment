48c3f351572ce709cbb0a0f28d06130617303e9f
Imsk31 <shubhamkalekar8805@gmail.com>
2026-03-27 18:42:34 +0530

    changes in output

--- terraform/modules/RDS/output.tf
+++ terraform/modules/RDS/output.tf
@@ -1,8 +1,6 @@
 output "RDS_Endpoint" {
-  description = "The endpoint of the RDS instance"
   value = aws_db_instance.main.endpoint
 }
 output "rds_sg_id" {
-  description = "The security group ID of the RDS instance"
   value = aws_security_group.rds_sg.id
 }
