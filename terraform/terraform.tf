bca8d220e38cb595c96160728de22f12697bd00c
Imsk31 <shubhamkalekar8805@gmail.com>
2026-03-31 17:26:29 +0530

    adding required providers

---
+++ terraform/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+  }
+}
