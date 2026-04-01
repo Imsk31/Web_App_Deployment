bb152fa6912557c2de9f542bab11ac56363913ec
Imsk31 <shubhamkalekar8805@gmail.com>
2026-04-01 17:42:36 +0530

    Provider Changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,18 +2,26 @@ provider "aws" {
   region = var.region
 }
 
-# Required for ESO Helm install
-provider "kubernetes" {
-  host                   = module.EKS.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.EKS.cluster_ca_certificate)
+provider "tls" {}
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = [
-      "eks", "get-token",
-      "--cluster-name", var.cluster_name,
-      "--region", var.region
-    ]
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
