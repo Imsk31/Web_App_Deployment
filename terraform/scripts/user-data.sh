588f4c7def6f049c13133d2d6918c8f4a359b822
Imsk31 <shubhamkalekar8805@gmail.com>
2026-04-05 22:33:25 +0530

    adding java jdk and terraform dependencies

--- terraform/scripts/user-data.sh
+++ terraform/scripts/user-data.sh
@@ -10,6 +10,10 @@ sudo ./aws/install
 # Clean up the AWS CLI installer
 rm -rf awscliv2.zip aws/
 
+# Java installation for jenkins
+sudo apt update
+sudo apt install openjdk-17-jre -y
+
 # Add Docker's official GPG key
 sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl
@@ -27,6 +31,11 @@ echo \
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# Install Terraform
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
@@ -39,5 +48,4 @@ kubectl version --client
 sudo apt-get install mysql-client -y
 
 # Add the docker group and user
-
 sudo usermod -aG docker $USER && newgrp docker
